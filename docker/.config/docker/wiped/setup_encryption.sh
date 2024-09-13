fe19d55024700cc5121c720017326b965968563b
ghostway <ghostway@tuta.io>
2024-09-13 12:05:14 +0300

    fixes

--- docker/.config/docker/wiped/setup_encryption.sh
+++ docker/.config/docker/wiped/setup_encryption.sh
@@ -2,7 +2,21 @@
 
 ENCRYPTED_FILE=/dev/shm/encrypted_disk.img
 ENCRYPTED_MOUNT=/home
-DISK_SIZE=100M
+DISK_SIZE=256M
+
+if losetup | grep '/dev/loop0' > /dev/null 2>&1; then
+    echo "Cleaning up previous loop device /dev/loop0..."
+    if cryptsetup status encrypted_volume > /dev/null 2>&1; then
+        echo "Closing previous encrypted volume..."
+        cryptsetup luksClose encrypted_volume || echo "Failed to close encrypted_volume."
+    fi
+    losetup -d /dev/loop0 || echo "Failed to detach loop device."
+fi
+
+if losetup | grep '/dev/loop0' > /dev/null 2>&1; then
+    echo "/dev/loop0 is still busy. Exiting."
+    exit 1
+fi
 
 KEY=$(head -c 32 /dev/urandom | base64)
 
@@ -21,11 +35,13 @@ echo "/home ($DISK_SIZE) is encrypted, and now your home. good luck!"
 export HOME=/home
 cd /home
 
-exec /bin/sh
+/bin/sh
 
-umount $ENCRYPTED_MOUNT
-cryptsetup luksClose encrypted_volume
-losetup -d /dev/loop0
-rm $ENCRYPTED_FILE
+cd /
+
+echo "cleaning up..."
+umount $ENCRYPTED_MOUNT || echo "failed to unmount $ENCRYPTED_MOUNT."
+cryptsetup luksClose encrypted_volume || echo "failed to close encrypted volume."
+losetup -d /dev/loop0 || echo "failed to detach loop device."
+rm -f $ENCRYPTED_FILE || echo "failed to remove encrypted file."
 echo "encrypted volume cleaned up and deleted."
-
